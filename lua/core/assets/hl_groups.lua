1f91572f6880e8ebeecc8ed426984fc3438ff3a5
q <mhawryl1@gmail.com>
2025-04-22 19:21:37 +0200

    added highlight color group for autosuggestion

--- lua/core/assets/hl_groups.lua
+++ lua/core/assets/hl_groups.lua
@@ -11,6 +11,6 @@ vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold =
 vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
 vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
 vim.api.nvim_set_hl(0, "MarkSignHL", { fg = "#C792EA", bg = "#3c3836" })
-
+vim.api.nvim_set_hl(0, "CmpGhostText", { fg = "#888888", italic = true })
 -- Change Highlight group text color for which-key if grouvbox theme is enabled
 if vim.g.colors_name == "gruvbox" then vim.api.nvim_set_hl(0, "WhichKeyGroup", { fg = "#82AAFF", bg = "NONE" }) end
